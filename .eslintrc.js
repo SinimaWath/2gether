b8507262a5d2c4da4ffcef36934dc043661bcdd8
Vladislav Tarasov <vlad-tarasov@yandex-team.ru>
2021-05-09 23:32:48 +0300

    Init Linters & Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    plugins: ['prettier', 'react'],
+    extends: ['plugin:prettier/recommended', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+    },
+    env: {
+        es6: true,
+        node: true,
+        browser: true,
+    },
+    parserOptions: {
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+};
